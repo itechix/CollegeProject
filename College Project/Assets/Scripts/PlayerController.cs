4cd1556cf876c3ced4f226d7151c702ebbab255a
Verox <verox.averre@gmail.com>
2014-12-20 10:22:47 +0000

    Implemented a better jump example

--- College Project/Assets/Scripts/PlayerController.cs
+++ College Project/Assets/Scripts/PlayerController.cs
@@ -19,17 +19,36 @@ public class PlayerController : MonoBehaviour {
 		// Creating floats to hold the speed of 
 		float playerSpeedHorizontal = 5f * Input.GetAxis ("Horizontal");
 		float playerSpeedVertical = 10f * Input.GetAxis ("Vertical");
-		float playerSpeedJump = 10f * Input.GetAxis ("Jump");
+
+
+
+		//float playerSpeedJump = 10f * Input.GetAxis ("Jump");
 		
 		// Transform statements to move the player by the playerSpeed amount.
 		transform.Translate (Vector3.forward * playerSpeedVertical * Time.deltaTime);
 		transform.Translate (Vector3.right * playerSpeedHorizontal * Time.deltaTime);
 		
-		Vector3 playerJumpCheck = transform.TransformDirection (Vector3.down);
-		
-		if(Physics.Raycast(transform.position, playerJumpCheck, playerSpeedJump)) { 
-			transform.Translate (Vector3.up* playerSpeedJump * Time.deltaTime);
-			Debug.Log ("You can jump");
+		//Vector3 playerJumpCheck = transform.TransformDirection (Vector3.down);
+
+		if (Input.GetButton("Jump"))
+		{
+			playerJump();
 		}
+		
+		//if(Physics.Raycast(transform.position, playerJumpCheck, playerSpeedJump)) { 
+		//	transform.Translate (Vector3.up* playerSpeedJump * Time.deltaTime);
+		//	Debug.Log ("You can jump");
+		//}
 	}
+
+	/// <summary>
+	/// Here we handle anything to do with the jump, including the raycast, any animations, and the force setting it's self.
+	/// </summary>
+	void playerJump() {
+
+		const float JumpForce = 100.0f;
+
+		rigidbody.AddForce (Vector3.up * JumpForce);
+	}
+
 }
